5f4de35394f3fd53ac666d5b1779738f3158a47b
curs3m4rk <pujyapreyans35@gmail.com>
2026-02-25 00:57:38 +0530

    Setup ApplicationDbContext

--- DIFC-Backend/DIFC.API/Program.cs
+++ DIFC-Backend/DIFC.API/Program.cs
@@ -1,8 +1,18 @@
+using DIFC.Domain.Entities;
+using DIFC.Infrastructure.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
 
 var app = builder.Build();
 
---
+++ DIFC-Backend/DIFC.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using DIFC.Domain.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DIFC.Infrastructure.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+    }
+}
