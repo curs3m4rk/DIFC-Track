3fe452ffb86f5b18b821ff0185776e26b3a5dd56
curs3m4rk <pujyapreyans35@gmail.com>
2026-03-15 00:18:50 +0530

    Update-databse setup in program cs and ci/cd setup

--- DIFC-Backend/DIFC.API/Program.cs
+++ DIFC-Backend/DIFC.API/Program.cs
@@ -162,5 +162,12 @@ app.UseSwagger();
 app.UseSwaggerUI();
 
 app.MapControllers();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    db.Database.Migrate();
+}
+
 app.Run();
 
